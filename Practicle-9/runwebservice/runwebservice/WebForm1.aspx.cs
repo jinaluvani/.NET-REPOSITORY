1aeb645bf2033e14a7df8eb39b45f4763988fde1
jinaluvani <46022527+jinaluvani@users.noreply.github.com>
2019-04-21 23:30:39 +0530

    update

---
+++ Practicle-9/runwebservice/runwebservice/WebForm1.aspx.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace runwebservice
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        trojan.WebService1 calc = new trojan.WebService1();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void add_Click(object sender, EventArgs e)
+        {
+
+            Label1.Text = calc.Add(Convert.ToInt16(input1.Text), Convert.ToInt16(input2.Text)).ToString();
+        }
+
+        protected void mul_Click(object sender, EventArgs e)
+        {
+
+            Label1.Text = calc.Mul(Convert.ToInt16(input1.Text), Convert.ToInt16(input2.Text)).ToString();
+        }
+
+        protected void sub_Click(object sender, EventArgs e)
+        {
+            Label1.Text = calc.Sub(Convert.ToInt16(input1.Text), Convert.ToInt16(input2.Text)).ToString();
+        }
+
+        protected void div_Click(object sender, EventArgs e)
+        {
+            Label1.Text = calc.Div(Convert.ToInt16(input1.Text), Convert.ToInt16(input2.Text)).ToString();
+        }
+    }
+}
+}
